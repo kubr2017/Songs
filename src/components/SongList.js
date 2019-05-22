85984c9b5a336f3e1eee9d4ec96f0daee5fbbd4d
r <kubr2017@gmail.com>
2019-05-22 12:12:53 -0400

    Feat: Add mapStateToProps

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,5 +1,5 @@
-import React, {Component} from 'react';
-import {connect} from 'react-redux';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class SongList extends Component {
   render(){
@@ -7,4 +7,8 @@ class SongList extends Component {
   }
 }
 
-export default connect()(SongList);
+const mapStateToProps = (state) => {
+  return { songs:state.songs }
+}
+
+export default connect(mapStateToProps)(SongList);
