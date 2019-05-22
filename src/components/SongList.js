f678d4738e235714e9b7f8dba68808c7593fcbff
r <kubr2017@gmail.com>
2019-05-22 12:58:10 -0400

    choar: add log

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -2,26 +2,30 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 class SongList extends Component {
+
   renderList(){
+    console.log('inside render list',this.props.songs);
     this.props.songs.map((song)=>{
       return (
         <div className='item' key = {song.title}>
-         <div className='right floated content'>
-           <button className='ui button primary'>
-             Select
-           </button>
-           <div className='content'>{song.title}</div>
-         </div>
+          <div className='right floated content'>
+            <button className='ui button primary'>
+              Select
+            </button>
+            <div className='content'>{song.title}</div>
+          </div>
         </div>
       );
     })
   }
+
   render(){
     return <div>{this.renderList()}</div>;
   }
 }
 
 const mapStateToProps = (state) => {
+  console.log(state.songs);
   return { songs:state.songs }
 }
 
